2ada01485d84766690c264349be0f834b99b4d43
srtianxia <srtianxia@163.com>
2016-02-10 22:14:15 +0800

    commit

--- app/src/main/java/com/srtianxia/zhibook/view/activity/ActivityAnswer.java
+++ app/src/main/java/com/srtianxia/zhibook/view/activity/ActivityAnswer.java
@@ -6,7 +6,6 @@ import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
